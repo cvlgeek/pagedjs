cc480a77450cd2eff686c20989ce77bfccc8ec97
cvlgeek <70110028+cvlgeek@users.noreply.github.com>
2023-04-03 15:37:59 -0600

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,14 @@
-    function previewDoc(){
-      var htmlSource = editor.getValue();
-      var htmlDoc = document.getElementById('preview').contentDocument;
-      htmlDoc.open();
-      htmlDoc.write(htmlSource);
-      htmlDoc.close();
-    }
+var editor = ace.edit('codeEditor');
+editor.session.setUseWorker(false);
+editor.setShowPrintMargin(false);
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/html");
+editor.session.on('change', function(){previewDoc()});    
+
+function previewDoc(){
+  var htmlSource = editor.getValue();
+  var htmlDoc = document.getElementById('preview').contentDocument;
+  htmlDoc.open();
+  htmlDoc.write(htmlSource);
+  htmlDoc.close();
+}
